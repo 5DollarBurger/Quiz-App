7e6728da5e4bdefbdaadca7b2baff3f0ff39c4d7
Darrell Leong <a843183@yara.com>
2021-07-06 11:28:46 +0800

    mapped question data to question screen

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,13 +3,33 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import com.example.quizapp.databinding.ActivityMainBinding
+import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        binding = ActivityQuizQuestionsBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
         val questionList = Constants.getQuestions()
         Log.i("Questions Size","${questionList.size}")
+
+        val currentPosition = 1
+        val question : Question? = questionList[currentPosition - 1]
+
+        binding.pb.progress = currentPosition
+        binding.tvProgress.text = "${currentPosition}/" + binding.pb.max
+
+        binding.ivImage.setImageResource(question!!.image)
+        binding.tvQuestion.text = question!!.question
+        binding.tvOption1.text = question!!.option1
+        binding.tvOption2.text = question!!.option2
+        binding.tvOption3.text = question!!.option3
+        binding.tvOption4.text = question!!.option4
     }
 }
